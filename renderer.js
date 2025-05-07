90fd4b534e571e5a8727fe4f81c12156267f5554
rayenomr123 <rayenomri23p@gmail.com>
2025-05-07 22:01:33 +0100

    push app

---
+++ renderer.js
@@ -0,0 +1,18 @@
+const information = document.getElementById('info')
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+
+const func = async () => {
+    const response = await window.versions.ping()
+    console.log(response) // prints out 'pong'
+  }
+  
+func()
+
+let count = 0;
+const countDisplay = document.getElementById('count');
+const incrementBtn = document.getElementById('incrementBtn');
+
+incrementBtn.addEventListener('click', () => {
+  count++;
+  countDisplay.innerText = count;
+});
